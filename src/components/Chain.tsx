543a2174857a6b55889c54919867ec7c1d0dcb5e
Tim <virtualxself@gmail.com>
2023-01-31 22:50:19 +0200

    move Chain component to .tsx

--- src/components/Chain.tsx
+++ src/components/Chain.tsx
@@ -6,28 +6,34 @@ import { Outlet, Link, useNavigate } from "react-router-dom";
 import AppComponentContext from "../contexts/AppComponentContext";
 import ChainComponentContext from "../contexts/ChainComponentContext";
 
+// Типизация
+import IChainProps from "../interfaces/IChainProps";
+import ICoin from "../interfaces/ICoin";
+import IProposal from "../interfaces/IProposal";
+import IChain from "../interfaces/IChain";
+
 // Мой код
 import CosmosRestApi from "../api/CosmosRestApi";
 import { cutDecimals, cutExtra, tweakPrice, filterActive, getPath } from "../utils/formatting";
 
 
 
-function Chain(props) {
+function Chain(props: IChainProps) {
 
   const coins = useContext(AppComponentContext).coins;
   const currentChain = props.chain;
   const setCurrentChain = useContext(AppComponentContext).setCurrentChain;
   const chainApi = new CosmosRestApi(currentChain.api[0]);
-  const [activeSetLength, setActiveSetLength] = useState(null);
-  const [wholeSetLength, setWholeSetLength] = useState(null);
-  const [totalBonded, setTotalBonded] = useState(null); // получается один раз для расчёта voting power валидаторов и больше не меняется
-  const [totalBondedUpdating, setTotalBondedUpdating] = useState(null); // то же, что и totalBonded, но обновляется по таймеру
-  const [unbondingTime, setUnbondingTime] = useState(null);
-  const [activeProposals, setActiveProposals] = useState(null);
-  const [blockHeight, setBlockHeight] = useState(null);
-  const [inflation, setInflation] = useState(null);
-  const [communityPool, setCommunityPool] = useState(null);
-  const [price, setPrice] = useState(null);
+  const [activeSetLength, setActiveSetLength] = useState<number | null>(null);
+  const [wholeSetLength, setWholeSetLength] = useState<number | null>(null);
+  const [totalBonded, setTotalBonded] = useState<string | null>(null); // получается один раз для расчёта voting power валидаторов и больше не меняется
+  const [totalBondedUpdating, setTotalBondedUpdating] = useState<string | null>(null); // то же, что и totalBonded, но обновляется по таймеру
+  const [unbondingTime, setUnbondingTime] = useState<number | null>(null);
+  const [activeProposals, setActiveProposals] = useState<string | null>(null);
+  const [blockHeight, setBlockHeight] = useState<string | null>(null);
+  const [inflation, setInflation] = useState<string | null>(null);
+  const [communityPool, setCommunityPool] = useState<string | null>(null);
+  const [price, setPrice] = useState<string | null>(null);
   const navigate = useNavigate();
 
   // ОБНОВЛЯЕМ ТЕКУЩУЮ СЕТЬ 
@@ -94,7 +100,7 @@ function Chain(props) {
   useEffect(() => {
     chainApi.getProposals()
       .then(result => {
-        const active = result.proposals.filter(p => p.status === 'PROPOSAL_STATUS_VOTING_PERIOD');
+        const active = result.proposals.filter((proposal: IProposal) => proposal.status === 'PROPOSAL_STATUS_VOTING_PERIOD');
         setActiveProposals(active);
       })
       .catch(() => setActiveProposals(null))
@@ -118,7 +124,7 @@ function Chain(props) {
   useEffect(() => {
     chainApi.getCommunityPool()
       .then(result => {
-        const pool = result.pool.find(el => el.denom === currentChain.denom);
+        const pool = result.pool.find((el: any) => el.denom === currentChain.denom);
         const amount = pool.amount;
         const cutted = cutExtra(amount, 19); // точка + 18 символов
         setCommunityPool(cutted);
@@ -129,7 +135,7 @@ function Chain(props) {
   // ПОЛУЧАЕМ ЦЕНУ ТОКЕНА
   useEffect(() => {
     if (coins && currentChain.coinGecko) {
-      const currentCoin = coins.find(coin => coin.id === currentChain.coinGecko);
+      const currentCoin = coins.find((coin: ICoin) => coin.id === currentChain.coinGecko);
       setPrice(currentCoin.current_price);
     } else {
       setPrice(null);
@@ -201,7 +207,7 @@ function Chain(props) {
   // РЕНДЕР ИНФЛЯЦИИ
   let inflationEl = errorEl;
   if (inflation) {
-    const value = (inflation * 100).toFixed(2) + '%';
+    const value = (Number(inflation) * 100).toFixed(2) + '%';
     inflationEl = <span className="chain__plate-data">{value}</span>;
   }
 
@@ -215,7 +221,7 @@ function Chain(props) {
   // РЕНДЕР ЦЕНЫ
   let priceEl = errorEl;
   if (price) {
-    const value = '$' + tweakPrice(price);
+    const value = '$' + tweakPrice(Number(price));
     priceEl = <a href={`https://www.coingecko.com/en/coins/${currentChain.coinGecko}`} target="_blank" className="chain__plate-link">{value}</a>;
   }
 
--- src/interfaces/IChainProps.ts
+++ src/interfaces/IChainProps.ts
@@ -2,7 +2,6 @@ import IChain from "./IChain";
 
 interface IChainProps {
   chain: IChain,
-  setCurrentChain: React.Dispatch<React.SetStateAction<IChain | null>>
 }
 
 export default IChainProps;
